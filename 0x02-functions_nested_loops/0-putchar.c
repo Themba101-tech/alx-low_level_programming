27011cbd8c58f2802c4ea2b19195954729051b9c
Themba101-tech <thembatheu@gmail.com>
2022-06-07 14:59:12 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/**
+ * * main - prints putcar
+ * * Return: Always 0
+*/
+
+int main(void)
+
+{	
+_putcar('_');
+_putcar('p');
+_putcar('u');
+_putcar('t');
+_putcar('c');
+_putcar('a');
+_putcar('r');
+_putcar('\n');
+return (0);
+}
