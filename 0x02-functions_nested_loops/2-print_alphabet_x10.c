078d32c68eb737e48c4102f6336e3f8725f646e0
Themba101-tech <thembatheu@gmail.com>
2022-06-08 16:42:44 -0700

    alpha10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * * print_alphabet_x10 
- * * Description: function uses _putchar function to print
- * * alphabet in lowercase 10 times
- * * Return: 0. success
+ * print_alphabet_x10
+ * Description: function uses _putchar function to print
+ * Return: 0. success
 */
 
 void print_alphabet_x10(void)
