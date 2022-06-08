05f91736e661873c94568c6184c11414f23a069a
Themba101-tech <thembatheu@gmail.com>
2022-06-08 06:08:38 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * * main - prints putcar
- * * Return: Always 0
+ * * main - prints putcar followed by a new line
+ * * Return: Always 0.
 */
 
 int main(void)
 
 {	
-char" putchar"[10] = "_putchar";
-int i;
-for (i = 0; i < 8; i++)
-{
-_putchar("putchar"[i]);
-}
-_putchar(10);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n')
+
 return (0);
 }
