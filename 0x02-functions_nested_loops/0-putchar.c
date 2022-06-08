8d6c7e2fd7206cdc16aa469152b2800db2070b89
Themba101-tech <thembatheu@gmail.com>
2022-06-08 06:19:03 -0700

    fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,11 +2,10 @@
 
 /**
  * * main - prints putcar followed by a new line
- * * Return: Always 0.
+ * * Return: 0 on success.
 */
 
 int main(void)
-
 {	
 _putchar('_');
 _putchar('p');
@@ -16,7 +15,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('\n')
-
+_putchar('\n');
 return (0);
 }
