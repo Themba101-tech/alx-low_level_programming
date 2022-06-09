94b739132818f4a988341d5bfc9aa6cda0f7232b
Themba101-tech <thembatheu@gmail.com>
2022-06-09 14:39:23 -0700

    print dates

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,7 +10,6 @@
  * Return: void
 */
 
-
 void print_remaining_days(int month, int day, int year)
 
 {
@@ -20,7 +19,6 @@ if (month >= 3 && day >= 60)
 {
 day++;
 }
-
 
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
