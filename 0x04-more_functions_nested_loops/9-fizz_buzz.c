c479362ee3a9b9ef44688c2b4c9b9758af20d2ec
Themba101-tech <thembatheu@gmail.com>
2022-06-10 14:18:40 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,21 +9,26 @@
 int main(void)
 {
 int num;
-
 while (num++ < 100)
+
 if ((num % 3 == 0) && (num % 5 == 0))
 printf("FizzBuzz ");
-lse if ((num % 3) == 0)
+
+else if ((num % 3) == 0)
 printf("Fizz ");
+
 else if ((num % 5) == 0)
 {
 if (num != 100)
 printf("Buzz ");
+
 else
 printf("Buzz");
 }
+
 else
 printf("%d ", num);
+
 printf("\n");
 return (0);
 }
