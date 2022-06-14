78d0209754798245f9251fa284defb47d0c83ec5
Themba101-tech <thembatheu@gmail.com>
2022-06-14 08:34:20 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: The string to be modified
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+int i = 0, j = 0;
+char tmp[20];
+
+while (*(s + i))
+{
+*(tmp + i) = *(s + i);
+i++;
+}
+i = i - 1;
+while (i >= 0)
+{
+*(s + i) = *(tmp + j);
+j++;
+i--;
+}
+}
